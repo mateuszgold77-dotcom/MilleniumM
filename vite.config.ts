de47b9a4eefbdebefc345da83f8ba13579a2a658
mateuszgold77-dotcom <mateusz.gold77@gmail.com>
2026-04-20 17:21:39 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,13 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
-// import { inspectAttr } from 'kimi-plugin-inspect-react'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   base: '/MilleniumM/',
   plugins: [react()],
-  server: {
-    port: 3000,
-  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-});
+})
