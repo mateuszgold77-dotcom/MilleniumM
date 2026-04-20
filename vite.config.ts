7ed4ba5684fcfb22f7d555d19fe1cc617102baf6
mateuszgold77-dotcom <mateusz.gold77@gmail.com>
2026-04-20 16:54:10 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { inspectAttr } from 'kimi-plugin-inspect-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/Millenium-Management/',
   plugins: [inspectAttr(), react()],
   server: {
     port: 3000,
@@ -16,4 +16,3 @@ export default defineConfig({
     },
   },
 });
-base: '/Millenium-Management/'
